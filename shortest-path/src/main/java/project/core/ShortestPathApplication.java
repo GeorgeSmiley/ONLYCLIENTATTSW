88345226ee00957d6565e828ccd24e97f2a1e14f
tom <tommasopuccetti@gmail.com>
2017-12-14 12:45:24 +0100

    setting the environment

---
+++ shortest-path/src/main/java/project/core/ShortestPathApplication.java
@@ -0,0 +1,12 @@
+package project.core;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ShortestPathApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ShortestPathApplication.class, args);
+	}
+}
