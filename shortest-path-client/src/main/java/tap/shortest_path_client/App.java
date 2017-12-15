9e52ddd4cbbb7a3eadcf0e9349e5fb1359ad66aa
Alex Foglia <alex.f1793@gmail.com>
2017-12-15 18:46:26 +0100

    Commented main class

--- shortest-path-client/src/main/java/tap/shortest_path_client/App.java
+++ shortest-path-client/src/main/java/tap/shortest_path_client/App.java
@@ -2,24 +2,19 @@ package tap.shortest_path_client;
 
 import java.io.IOException;
 
-import com.google.gson.Gson;
 
 
 
 
-/**
- * Hello world!
- *
- */
 public class App 
 {
     public static void main( String[] args ) throws IOException
     {
-    	Gson gson=new Gson();
+    	/**
         Client cl=new Client();
         cl.setRestServiceClient(new RestServiceClient("http://localhost:8080","http://localhost:8080/path"));
         System.out.println((cl.getShortestPath("node1","node2")));
-
+		**/
        
         
     }
